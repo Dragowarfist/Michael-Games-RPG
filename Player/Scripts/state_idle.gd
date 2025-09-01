ab8bc0fea463a4dbf83f0f449348070ee8eef5d7
Dragowarfist <231frederick@gmail.com>
2025-09-01 18:09:49 +0800

    Initial commit

---
+++ Player/Scripts/state_idle.gd
@@ -0,0 +1,34 @@
+class_name State_Idle extends State
+
+@onready var walk: State_Walk = $"../Walk"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+## What happens when the player enters this state
+func Enter() -> void:
+	player.UpdateAnimation("idle")
+	pass
+
+
+## What happens when the player exits this state
+func Exit() -> void:
+	pass
+
+## What happens during the _process update in this state
+func Process(_delta: float ) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity = Vector2.ZERO
+	return null
+
+## What happens during the _physics process update in this State
+func Physics(_delta : float) -> State:
+	return null
+
+## What happens with input events in this State
+func HandleInput( _event : InputEvent) -> State:
+	return null
---
+++ Player/Scripts/state_walk.gd
@@ -0,0 +1,44 @@
+class_name State_Walk extends State
+
+@export var move_speed : float = 100.0
+
+@onready var idle: State_Idle = $"../Idle"
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+## What happens when the player enters this state
+func Enter() -> void:
+	player.UpdateAnimation("walk")
+	pass
+
+
+## What happens when the player exits this state
+func Exit() -> void:
+	pass
+
+## What happens during the _process update in this state
+func Process(_delta: float ) -> State:
+	if player.direction == Vector2.ZERO:
+		return idle
+	
+	player.velocity = player.direction * move_speed
+	
+	if player.SetDirection():
+		Enter()
+	
+	
+	return null
+
+## What happens during the _physics process update in this State
+func Physics(_delta : float) -> State:
+	return null
+
+## What happens with input events in this State
+func HandleInput( _event : InputEvent) -> State:
+	return null
